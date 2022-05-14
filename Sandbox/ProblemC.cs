9d27df0320a13bfde979b6358991295cb13ca9c8
Vladimir Melkumov <melqumov@gmail.com>
2022-05-14 21:52:33 +0300

    Added the method for testing ProblemC.

--- Sandbox/ProblemC.cs
+++ Sandbox/ProblemC.cs
@@ -1,47 +1,72 @@
-using System.Diagnostics;
-
 namespace Sandbox
 {
-    static class ProblemC
-    {
-        public static void MySolution(string[] args)
-        {
-            //var k = int.Parse(Console.ReadLine());
-            var k = int.Parse(args[0]);
-            //var encodedArray = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            var encodedArray = args[1].Split(" ").Select(int.Parse).ToArray();
+	static class ProblemC
+	{
+		public static void ConsoleInput()
+		{
+			var k = Console.ReadLine();
+			var encodedArray = Console.ReadLine();
 
-            var decodedArray = new int[k + 1];
+			Console.WriteLine(MySolution(k, encodedArray));
+		}
 
-            var indexOfZero = 0;
+		public static void Test()
+		{
+			var testFiles = Directory.GetFiles(@"..\..\..\TestData\ProblemC");
 
-            var first = 0;
-            for (var i = 0; i < encodedArray.Length; i++)
-            {
-                var delta = encodedArray[i];
-                var second = first + delta;
+			foreach (var testInput in testFiles.Where(tf => !tf.EndsWith(".a")))
+			{
+				Console.WriteLine($"Test {testInput.Substring(testInput.Length - 2, 2)}:");
+				var inputs = File.ReadAllLines(testInput);
 
-                if (second < 0)
-                {
-                    indexOfZero = i + 1;
-                    continue;
-                }
+				var result = MySolution(inputs[0], inputs[1]);
 
-                first = second;
-                decodedArray[i + 1] = first;
-            }
+				var testResult = testFiles.FirstOrDefault(tf => tf == testInput + ".a");
 
-            var last = 0;
-            for (var j = indexOfZero - 1; j >= 0; j--)
-            {
-                var delta = encodedArray[j];
-                var previous = last - delta;
+				if (result == File.ReadAllText(testResult).Trim())
+					Console.WriteLine("Passed.");
+				else
+					Console.WriteLine("Not passed.");
+			}
+		}
 
-                last = previous;
-                decodedArray[j] = last;
-            }
+		public static string MySolution(string kAsString, string encodedArrayAsString)
+		{
+			var k = int.Parse(kAsString);
+			var encodedArray = encodedArrayAsString.Split(" ").Select(int.Parse).ToArray();
 
-            Console.WriteLine(string.Join(" ", decodedArray));
-        }
-    }
+			var decodedArray = new int[k + 1];
+
+			var indexOfZero = 0;
+
+			var first = 0;
+			for (var i = 0; i < encodedArray.Length; i++)
+			{
+				var delta = encodedArray[i];
+				var second = first + delta;
+
+				if (second < 0)
+				{
+					indexOfZero = i + 1;
+					first = 0;
+					continue;
+				}
+
+				first = second;
+				decodedArray[i + 1] = first;
+			}
+
+			var last = 0;
+			for (var j = indexOfZero - 1; j >= 0; j--)
+			{
+				var delta = encodedArray[j];
+				var previous = last - delta;
+
+				last = previous;
+				decodedArray[j] = last;
+			}
+
+			return string.Join(" ", decodedArray);
+		}
+	}
 }
